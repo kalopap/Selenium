f3a0c9af534985a19830a4c2a6aa36eee98b7d9f
kalyani <reachkalyanihere@hotmail.com>
2024-03-22 13:59:04 -0500

    end to end testcase updated

--- src/main/java/com/utilities/TestSettings.java
+++ src/main/java/com/utilities/TestSettings.java
@@ -4,15 +4,22 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestSettings {
 	
 	public static final String URL = "https://rahulshettyacademy.com/client";
 	WebDriver driver = new ChromeDriver();
-	
+	//WebDriver driver = new FirefoxDriver();
+	WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
 	
 	
 	//invoke the browser 
@@ -20,6 +27,7 @@ public class TestSettings {
 		
 		System.out.println("Launching the website..");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
 		driver.get(URL);		
 	}
 	
@@ -31,6 +39,10 @@ public class TestSettings {
 	public WebElement findById(String locator) {
 		return driver.findElement(By.id(locator));
 		
+	}
+	
+	public WebElement findByXpath(String locator) {
+		return driver.findElement(By.xpath(locator));
 	}
 	
 	public void typeInTextBox(WebElement elementInPage,String textToType) {
@@ -48,7 +60,44 @@ public class TestSettings {
 	public WebElement findByCssSelector(String locator) {
 		return driver.findElement(By.cssSelector(locator));
 	}
+	
+	public void findElementFromInputList(String locator) {
+		
+	}
+	
+	public void scrollToTheElementAndClick(String locator) {
+		WebElement element = driver.findElement(By.cssSelector(locator));
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", element);
+	}
+	
+	public void scrollElementIntoView(String locator) {
+		WebElement element = driver.findElement(By.cssSelector(locator));
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].scrollIntoView(true);", element);
+	}
+	
+	public void scrollDownThePage() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+	}
+	
+	public void waitForWebElementToBeVisible(String locator) {
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(locator)));
+	}
+	
+	public void waitForWebElementToBeInvisible(String locator) {
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(locator))));
+	}
+	
+	public void waitForElementToBeClickable(String locator) {
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(locator)));
+	}
+	
 	public void cleanUp() {
 		driver.close();
 	}
+	
 }
--- src/test/java/com/kalopap/MainTest.java
+++ src/test/java/com/kalopap/MainTest.java
@@ -3,7 +3,9 @@ package com.kalopap;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import com.utilities.TestSettings;
 
@@ -13,12 +15,19 @@ public class MainTest {
 	static WebElement emailId;
 	static WebElement password;
 	static WebElement loginBtn;
+	static WebElement country;
+	static WebElement checkoutBtn;
 	
 	static List<WebElement> products;
+	static List<WebElement> itemsInCart;
 	
 	public static void main(String[] args) {
+		
 		login();
 		AddItemToCart();
+		verifyItemsInCartAndCheckout();
+		placeOrder();
+		verifyFinalOrder();
 		//setup.cleanUp();
 	}
 	
@@ -41,24 +50,71 @@ public class MainTest {
 	}
 	
 	public static void AddItemToCart() {
+		
+		String productName;
+		//wait for all the products to be visible
+		setup.waitForWebElementToBeVisible(".mb-3");
 		//get all the products present
 		products = setup.findListByCssSelector(".mb-3");
 
 		//iterate through the elements
+		System.out.println("Total products found -" + products.size());
 
-		for(WebElement item : products) {
-			if(item.getText().equals("ADIDAS ORIGINAL")) {
-				item.click();
-				break;
+		for(WebElement product : products) {
+			productName = product.findElement(By.cssSelector("b")).getText();
+			System.out.println(productName);
+			if(productName.equals("ADIDAS ORIGINAL") || productName.equals("IPHONE 13 PRO")) {
+				//click on Add to Cart button
+				product.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+				
+				//wait for the 'Product added to cart' toast notification to appear after the item is added to cart
+				setup.waitForWebElementToBeVisible("#toast-container");
+				
+				//wait for the 'loading' ring to go invisible
+				setup.waitForWebElementToBeInvisible(".ng-animating");					
 			}			
 		}
-		//click on Add to Cart button
-		setup.findByCssSelector(".card-body button:last-of-type").click(); 
 		
-		//other way using Javastreams
-		/*WebElement actualProduct = products.stream().filter(product->product.findElement(By.cssSelector("b"))
-				.getText().equals("ADIDAS ORIGINAL")).findFirst().orElse(null);
-		actualProduct.findElement(By.cssSelector(".card-body button:last-of-type")).click();*/
+		//CLick on the 'Cart' menu on the top
+		System.out.println("CLick on the 'Cart' menu on the top");
+		setup.findByCssSelector("[routerlink*='cart']").click();
 	}
 	
+	public static void verifyItemsInCartAndCheckout() {
+		//get the list of item names in the cart
+		itemsInCart = setup.findListByCssSelector(".cartSection h3");
+		for(WebElement item : itemsInCart) {
+			//if(item.getText().equals("IPHONE 13 PRO") || item.getText().equals("ADIDAS ORIGINAL")) {
+				Assert.assertTrue(item.getText().equals("IPHONE 13 PRO") || item.getText().equals("ADIDAS ORIGINAL"));
+				System.out.println("The test for item "+ item.getText() + " is passed");
+			//}
+		}
+		
+	}
+	
+	public static void placeOrder() {
+	
+		setup.scrollToTheElementAndClick(".totalRow button");
+		country = setup.findByCssSelector("input[placeholder*='Select Country']");
+		setup.typeInTextBox(country, "Ind");
+		
+		List<WebElement> cntryList = setup.findListByCssSelector(".ta-results button span");
+		System.out.println(cntryList.size());
+		for(WebElement country : cntryList) {
+			if(country.getText().equals("India")) {					
+				country.click();	
+				break;
+			}
+		}
+		//click on Place order button
+		setup.scrollToTheElementAndClick(".action__submit");
+	}
+	
+	public static void verifyFinalOrder() {
+		String finalMsg = setup.findByCssSelector(".hero-primary").getText();
+		System.out.println(finalMsg);
+		Assert.assertTrue(finalMsg.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		setup.cleanUp();
+		
+	}
 }
